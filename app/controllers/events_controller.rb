97a72af9c43c66ed6a39178d6934465995fbb1e0
aellis928 <mrskawasaki.90@gmail.com>
2016-08-26 08:37:56 -0700

    correct mailer

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,9 @@ class EventsController < ApplicationController
   	time =Time.new
   	@time = time.strftime("%A")
   	@time_date = time.strftime("%B %d")
-    @events = @user.events
+    @time_compare = time.strftime ("%Y-%m-%d")
+    events = @user.events
+
     
 
     # so right now I need to figure out how to access the hour of each event? possibly using the above code or a variation of it.  Know that front-end wise, it will be looping through all the posted events of that particular user
@@ -28,10 +30,14 @@ class EventsController < ApplicationController
     # puts params['user_id']
     @user = User.find(params['user_id'])
     @event = Event.new( event_params )
-    
+
     if @event.save
-      CalendarMailer.event_email(@user).deliver
+      if !@event.notify
+        redirect_to("/events/#{session[:user_id]}")
+      else
+        CalendarMailer.event_email(@user).deliver
       redirect_to("/events/#{session[:user_id]}")
+      end
     else
       flash[:errors] = @event.errors.full_messages
       redirect_to("/events/#{session[:user_id]}")
@@ -42,9 +48,10 @@ class EventsController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params['user_id'])  
     @event = Event.find(params[:id])
     @event.destroy
-    redirect_to("events/#{session[:user_id]}")
+    redirect_to("/events/#{session[:user_id]}")
   end
 
   def logout
@@ -59,3 +66,11 @@ class EventsController < ApplicationController
       params.require(:event).permit(:user_id, :name, :description, :location, :date, :time, :color, :notify)
     end
 end
+
+# get the events to appear on the correct date!!
+
+# update the email text! need to also pass the event through as a parameter!
+
+# maybe some javascript to show details of each of the event?  
+
+# then its just overall styling of the websight! woohoo! Then all done!
